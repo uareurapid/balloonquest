ea14ea16816334141c5a2978e32cb788cdb5abec
uareurapid <cristo.paulo@gmail.com>
2015-06-30 00:44:04 +0100

    add 8 levels

---
+++ Assets/scripts/GroundScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundScript : MonoBehaviour {
+
+	private bool isVisible = false;
+	private MovingPlatformScript platformScript;
+	// Use this for initialization
+	void Start () {
+	
+	   platformScript = GetComponent<MovingPlatformScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnBecameVisible() {
+		
+		//disable movement when collider becames visible
+		isVisible = true;
+		if(platformScript!=null) {
+		  Invoke("StopMovement",1.0f);
+		}
+	}
+	
+	void StopMovement() {
+		platformScript.enabled = false;
+	}
+	
+	void OnBecameInvisible() {
+	
+	}
+}
--- Assets/scripts/MainLoaderScript.cs
+++ Assets/scripts/MainLoaderScript.cs
@@ -23,5 +23,20 @@ public class MainLoaderScript : MonoBehaviour {
 		if (GUI.Button(new Rect(10,130,50,30),"Level3")) {
 			Application.LoadLevel("Level3");
 		}
+		if (GUI.Button(new Rect(10,160,50,30),"Level4")) {
+			Application.LoadLevel("Level4");
+		}
+		if (GUI.Button(new Rect(10,190,50,30),"Level5")) {
+			Application.LoadLevel("Level5");
+		}
+		if (GUI.Button(new Rect(10,220,50,30),"Level6")) {
+			Application.LoadLevel("Level6");
+		}
+		if (GUI.Button(new Rect(10,250,50,30),"Level7")) {
+			Application.LoadLevel("Level7");
+		}
+		if (GUI.Button(new Rect(10,280,50,30),"Level8")) {
+			Application.LoadLevel("Level8");
+		}
 	}
 }
