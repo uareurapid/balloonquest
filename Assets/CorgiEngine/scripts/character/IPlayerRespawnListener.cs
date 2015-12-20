a6acf85e3f1e3e0c54845affcc371e28797ba7f4
PC Dreams (Paulo Cristo) <cristo.paulo@gmail.com>
2015-12-20 22:11:38 +0000

    rm corbi engine files

--- Assets/CorgiEngine/scripts/character/IPlayerRespawnListener.cs
+++
@@ -1,7 +0,0 @@
-/// <summary>
-/// Interface for player respawn
-/// </summary>
-public interface IPlayerRespawnListener
-{
-	void onPlayerRespawnInThisCheckpoint(CheckPoint checkpoint, CharacterBehavior player);
-}
